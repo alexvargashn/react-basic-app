011482fdc42683a95b71e7988d6f8176e4542f29
Alex Vargas <alexv7142@gmail.com>
2023-12-01 22:11:45 -0600

    Login Page seted

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,10 +2,39 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 
 const LoginPage = () => {
-    const { register } = useForm();
-    return (
-        <div>
 
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data)
+    })
+
+    return (
+        <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
+            <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+                <h1 className='text-2xl font-bold'>Login</h1>
+                <form onSubmit={onSubmit}>
+                    <input
+                        className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                        type="email" placeholder='email'
+                        {...register('email', { required: true })}
+                    />
+                    {errors.email && <p className='text-red-500'> Email is required</p>}
+                    <input
+                        className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
+                        type="password" name="password" placeholder='password'
+                        {...register('password', { required: true })}
+                    />
+                    {errors.password && <p className='text-red-500'> Password is required</p>}
+                    <button type="submit">
+                        LogIn
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
